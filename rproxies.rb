6c1f0e3009f5eba4182b7ce22f8dd13adb2e74c0
allergo <svalkanov97@gmail.com>
2020-12-28 12:51:28 +0200

    Remove unused method

--- rproxies.rb
+++ rproxies.rb
@@ -30,10 +30,6 @@ def user_agent
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 end
 
-def random
-  Random.new
-end
-
 def anonimity_levels
   { 'high' => "#{green 'elite'}", 'medium' => "#{yellow 'anonymous'}", 'low' => 'transparent' }
 end
